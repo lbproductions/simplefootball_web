39b0d00d2fa54f5731a2bda2ac948486cfb303a5
Niclas Raabe <niclas.raabe@grandcentrix.net>
2019-03-05 00:24:52 +0100

    Reading html from file

--- test/simplefootball_web_web/scraper_test.exs
+++ test/simplefootball_web_web/scraper_test.exs
@@ -6,16 +6,33 @@ defmodule SimplefootballWebWeb.ScraperTest do
   require Logger
 
   test "scrape" do
-    html =
-      HTTPoison.get!(
-        "https://www.transfermarkt.de/1-bundesliga/spieltag/wettbewerb/L1/plus/0?saison_id=2018&spieltag=18"
-      ).body
+    {:ok, result} =
+      File.read("./test/simplefootball_web_web/resources/tm/bundesliga_2018_18.html")
 
-    document = Meeseeks.parse(html)
-    result = Meeseeks.all(document, xpath("//table[@style='border-top: 0 !important;']"))
+    # html =
+    #   HTTPoison.get!(
+    #     "https://www.transfermarkt.de/1-bundesliga/spieltag/wettbewerb/L1/plus/0?saison_id=2018&spieltag=18"
+    #   ).body
+
+    document = Meeseeks.parse(result)
+    gameTables = Meeseeks.all(document, xpath("//table[@style='border-top: 0 !important;']"))
+    result = Enum.map(gameTables, fn table -> match(table) end)
+  end
+
+  def match(table) do
+    names =
+      Meeseeks.all(table, xpath(".//a[@class='vereinprofil_tooltip']"))
+      |> Enum.map(fn name -> Meeseeks.text(name) end)
+      |> Enum.filter(fn name -> name != "" end)
+      |> split()
 
     Logger.debug(fn ->
-      "result: #{inspect(result)}"
+      "names: #{inspect(names)}"
     end)
+  end
+
+  def split(list) do
+    len = round(length(list) / 2)
+    Enum.split(list, len)
   end
 end
